c15c07b785f7dc9839a525280ef0957f1d9b17c6
karroumingi4-art <karroumingi4@gmail.com>
2026-08-07 15:43:44 +0200

    Refactor AcademyPage to use createFileRoute

--- src/routes/academy.tsx
+++ src/routes/academy.tsx
@@ -1,5 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
 import { Academy } from "../components/site/Academy";
 
-export default function AcademyPage() {
+export const Route = createFileRoute("/academy")({
+  component: AcademyPage,
+});
+
+function AcademyPage() {
   return <Academy />;
 }
