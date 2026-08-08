524a5cc12dfa6e0cd9303718eca72e6d772cdc36
karroumingi4-art <karroumingi4@gmail.com>
2026-08-08 19:03:15 +0200

    Update Squad.tsx

--- src/components/site/Squad.tsx
+++ src/components/site/Squad.tsx
@@ -39,6 +39,8 @@ const players: Player[] = [
   { number: "27", name: "Christian Vieri", position: "Attaccante", image: "https://unsplash.com" },
   { number: "28", name: "Tariq Mansoor", position: "Attaccante", image: "https://unsplash.com" },
   { number: "99", name: "Gabriele Rossi", position: "Attaccante", image: "https://unsplash.com" }
+  { number: "0", name: "Alberto Malesani", position: "Mister", image: "public/malesani.png" },
+  { number: "99", name: "Gabriele Rossi", position: "Attaccante", image: "https://unsplash.com" }
 ];
 
 export function Squad() {
