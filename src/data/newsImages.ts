21499e06f2e88726ff15874f62dd15ed7c28aceb
karroumingi4-art <karroumingi4@gmail.com>
2026-08-15 17:34:15 +0200

    Add newsImages.ts for image mapping

---
+++ src/data/newsImages.ts
@@ -0,0 +1,9 @@
+import n1 from "@/assets/news-1.jpg";
+import n2 from "@/assets/gaston-villa-maglia (2).jpg";
+import n3 from "@/assets/news-3.jpg";
+
+export const imageMap: any = {
+  "news-1": n1,
+  "maglia": n2,
+  "news-3": n3,
+};
