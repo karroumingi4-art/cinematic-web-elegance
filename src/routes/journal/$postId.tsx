cd7b491d86b3e74a51b57d24a41793bdf79cc21a
karroumingi4-art <karroumingi4@gmail.com>
2026-08-15 23:28:20 +0200

    Fix state declaration syntax in MagliaSlides

--- src/routes/journal/$postId.tsx
+++ src/routes/journal/$postId.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute("/journal/$postId")({
 });
 
 function MagliaSlides() {
-  const [slide][setSlide] = useState(0);
+const [slide, setSlide] = useState(0);
   useEffect(() => {
     const t = setInterval(() => setSlide(s => (s+1)%3), 3000);
     return () => clearInterval(t);
