58f9f138a071696aefad545cb0a59e68835e3d9c
karroumingi4-art <karroumingi4@gmail.com>
2026-08-15 22:55:31 +0200

    Refactor imageMap to use Record type and update paths

--- src/data/newsImages.ts
+++ src/data/newsImages.ts
@@ -1,14 +1,8 @@
-import n1 from "@/assets/news-1.jpg";
-import n2 from "@/assets/gaston-villa-maglia (2).jpg";
-import n3 from "@/assets/news-3.jpg";
-import gazzetta1 from "@/assets/gazzetta-fanta-1.jpg";
-import gazzetta2 from "@/assets/gazzetta-fanta-2.jpg";
-"maglia-trasparente": "/images/gaston-villa-maglia-trasparente.png"
-
-export const imageMap: any = {
-  "news-1": n1,
-  "maglia": n2,
-  "news-3": n3,
-  "gazzetta-fanta-1": gazzetta1,
-  "gazzetta-fanta-2": gazzetta2,
+export const imageMap: Record<string, string> = {
+  "gazzetta-fanta-1": "/images/gazzetta-fanta-1.jpg",
+  "gazzetta-fanta-2": "/images/gazzetta-fanta-2.jpg",
+  "news-1": "/images/news-1.jpg",
+  "news-3": "/images/news-3.jpg",
+  "maglia": "/images/maglia.jpg",
+  "maglia-trasparente": "/images/maglia-trasparente.png",
 };
