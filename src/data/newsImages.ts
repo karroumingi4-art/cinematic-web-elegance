907f1c8a480c188912815693af45036571d30c87
karroumingi4-art <karroumingi4@gmail.com>
2026-08-15 23:16:58 +0200

    Fix image paths in newsImages.ts
    Updated image paths to include '/images/' prefix.

--- src/data/newsImages.ts
+++ src/data/newsImages.ts
@@ -1,11 +1,8 @@
-// src/data/newsImages.ts - FIX per foto in public/ o public/files/
 export const imageMap: Record<string, string> = {
-  "gazzetta-fanta-1": "/gazzetta-fanta-1.jpg",
-  "gazzetta-fanta-2": "/gazzetta-fanta-2.jpg",
-  "news-1": "/news-1.jpg",
-  "news-3": "/news-3.jpg",
-  "maglia": "/gaston-villa-maglia_2_-LDGmoDta.jpg",
-  "maglia-trasparente": "/maglia-trasparente.png",
-  "gaston-villa-maglia_2_-ldgmodta": "/gaston-villa-maglia_2_-LDGmoDta.jpg",
-  "gaston-villa-maglia": "/gaston-villa-maglia_2_-LDGmoDta.jpg",
+  "gazzetta-fanta-1": "/images/gazzetta-fanta-1.jpg",
+  "gazzetta-fanta-2": "/images/gazzetta-fanta-2.jpg",
+  "news-1": "/images/news-1.jpg",
+  "news-3": "/images/news-3.jpg",
+  "maglia": "/images/gaston-villa-maglia_2_-LDGmoDta.jpg",
+  "maglia-trasparente": "/images/maglia-trasparente.png",
 };
