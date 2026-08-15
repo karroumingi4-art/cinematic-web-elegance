2375df26aea2d73c36d785a6d78765bd8b0106d5
karroumingi4-art <karroumingi4@gmail.com>
2026-08-15 21:35:17 +0200

    Add new gazzetta images to imageMap

--- src/data/newsImages.ts
+++ src/data/newsImages.ts
@@ -1,9 +1,13 @@
 import n1 from "@/assets/news-1.jpg";
 import n2 from "@/assets/gaston-villa-maglia (2).jpg";
 import n3 from "@/assets/news-3.jpg";
+import gazzetta1 from "@/assets/gazzetta-fanta-1.jpg";
+import gazzetta2 from "@/assets/gazzetta-fanta-2.jpg";
 
 export const imageMap: any = {
   "news-1": n1,
   "maglia": n2,
   "news-3": n3,
+  "gazzetta-fanta-1": gazzetta1,
+  "gazzetta-fanta-2": gazzetta2,
 };
