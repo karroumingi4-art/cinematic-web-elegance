f767d603c57fcd88db9d1feef9cde6f249f08f77
karroumingi4-art <karroumingi4@gmail.com>
2026-08-18 21:39:21 +0200

    Add tribunale images mapping in tribunaleimages.ts

---
+++ src/data/tribunaleimages.ts
@@ -0,0 +1,11 @@
+import oro1 from "@/assets/tribunale-oro-1.jpg";
+import oro2 from "@/assets/tribunale-oro-2.jpg";
+import rossi from "@/assets/rossi.jpg";
+import bianchi from "@/assets/bianchi.jpg";
+
+export const tribunaleImageMap: any = {
+  "tribunale-oro-1": oro1,
+  "tribunale-oro-2": oro2,
+  "rossi-cap": rossi,
+  "bianchi-cap": bianchi
+};
